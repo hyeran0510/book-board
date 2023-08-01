6abf393e746f46bd9cb8a684e991d0edfb1d0185
hyeran0510 <132330609+hyeran0510@users.noreply.github.com>
2023-08-01 14:48:52 +0900

    Initial commit

---
+++ src/main/java/com/re/book/board/BookBoardApplication.java
@@ -0,0 +1,15 @@
+package com.re.book.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class BookBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/re/book/board/BookBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re.book.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
